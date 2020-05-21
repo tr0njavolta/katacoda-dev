15d54c9e35da7f1d215f315fc38ee86ef09c04d2
Rachel Sharp <rachel@hashicorp.com>
2020-05-21 12:33:15 -0400

    mv

--- terraform-sentinel/courseBase1.sh
+++ terraform-sentinel/courseBase1.sh
@@ -13,4 +13,9 @@ unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 curl -O https://releases.hashicorp.com/sentinel/0.15.5/sentinel_0.15.5_linux_amd64.zip
 unzip sentinel_0.15.5_linux_amd64.zip -d /usr/local/bin/
 
+mv fail.json pass.json policies/test/require-fizz
+mv mock-tfconfig.sentinel mock-tfplan-v2-pass.sentinel mock-tfplan.sentinel mock-tfstate-v2.sentinel mock-tfconfig-v2.sentinel mock-tfplan-v2-fail.sentinel mock-tfplan-v2.sentinel mock-tfrun.sentinel mock-tfstate.sentinel policies/testdata
+mv sentinel.json policies
+mv require-fizz.sentinel policies
+
 echo "Ready!"
