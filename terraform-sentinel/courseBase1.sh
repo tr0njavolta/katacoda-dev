866df370707a50e6017f3e2c8869e8ec765ff7d2
Rachel Sharp <rachel@hashicorp.com>
2020-05-21 14:39:54 -0400

    mv more assets directly

--- terraform-sentinel/courseBase1.sh
+++ terraform-sentinel/courseBase1.sh
@@ -3,6 +3,7 @@ cd ~/terraform-sentinel
 
 mkdir -p policies/test/require-fizz
 mkdir -p policies/testdata
+mkdir -p assets/assets
 
 # Install unzip - Katacoda docker image doesn't have unzip
 apt-get install unzip
@@ -13,9 +14,15 @@ unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 curl -O https://releases.hashicorp.com/sentinel/0.15.5/sentinel_0.15.5_linux_amd64.zip
 unzip sentinel_0.15.5_linux_amd64.zip -d /usr/local/bin/
 
-mv fail.json pass.json policies/test/require-fizz
-mv mock-tfconfig.sentinel mock-tfplan-v2-pass.sentinel mock-tfplan.sentinel mock-tfstate-v2.sentinel mock-tfconfig-v2.sentinel mock-tfplan-v2-fail.sentinel mock-tfplan-v2.sentinel mock-tfrun.sentinel mock-tfstate.sentinel policies/testdata
-mv sentinel.json policies
-mv require-fizz.sentinel policies
+cd ..
+
+mv fail.json pass.json ~/terraform-sentinel/policies/test/require-fizz
+mv mock-tfconfig.sentinel mock-tfplan-v2-pass.sentinel mock-tfplan.sentinel mock-tfstate-v2.sentinel mock-tfconfig-v2.sentinel mock-tfplan-v2-fail.sentinel mock-tfplan-v2.sentinel mock-tfrun.sentinel mock-tfstate.sentinel ~/terraform-sentinel/policies/testdata
+mv sentinel.json ~/terraform-sentinel/policies
+mv require-fizz.sentinel ~/terraform-sentinel/policies
+mv index.html ~/terraform-sentinel/assets/assets
+mv main.tf ~/terraform-sentinel/assets
+
+clear
 
 echo "Ready!"
