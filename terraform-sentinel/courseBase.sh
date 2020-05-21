eb93b6dcba431bb89c01491570d041d214b93820
Rachel Sharp <rachel@hashicorp.com>
2020-05-21 11:46:58 -0400

    assets in index

--- terraform-sentinel/courseBase.sh
+++ terraform-sentinel/courseBase.sh
@@ -18,7 +18,4 @@ unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 curl -O https://releases.hashicorp.com/sentinel/0.15.5/sentinel_0.15.5_linux_amd64.zip
 unzip sentinel_0.15.5_linux_amd64.zip -d /usr/local/bin/
 
-# Run Docker Compose up (daemon)
-docker-compose up -d
-
 echo "Ready!"
