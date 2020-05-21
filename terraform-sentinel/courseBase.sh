324bddd8838c11dd525e9f75f8afa4d92534fa37
Rachel Sharp <rachel@hashicorp.com>
2020-05-20 22:52:20 -0400

    mv & create policy folders

--- terraform-sentinel/courseBase.sh
+++ terraform-sentinel/courseBase.sh
@@ -1,6 +1,13 @@
- mkdir -p ~/terraform-sentinel
-$ cd ~/terraform-sentinel
+mkdir -p ~/terraform-sentinel
+cd ~/terraform-sentinel
 
+mkdir -p policies/test/require-fizz
+mkdir -p policies/testdata
+
+mv fail.json pass.json policies/test/require-fizz
+mv mock-tfconfig.sentinel mock-tfplan-v2-pass.sentinel mock-tfplan.sentinel mock-tfstate-v2.sentinel mock-tfconfig-v2.sentinel mock-tfplan-v2-fail.sentinel mock-tfplan-v2.sentinel mock-tfrun.sentinel mock-tfstate.sentinel policies/testdata
+mv sentinel.json policies
+mv require-fizz.sentinel policies
 
 # Install unzip - Katacoda docker image doesn't have unzip
 apt-get install unzip
