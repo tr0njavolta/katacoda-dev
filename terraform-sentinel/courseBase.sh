c7f9834776dd6542d59ee139219c0d4cdae0ce05
Rachel Sharp <rachel@hashicorp.com>
2020-05-20 21:10:59 -0400

    cleanup base & step1

--- terraform-sentinel/courseBase.sh
+++ terraform-sentinel/courseBase.sh
@@ -1,10 +1,12 @@
+ mkdir -p ~/terraform-sentinel
+$ cd ~/terraform-sentinel
+
 # Install Terraform and init config
 # Install docker - Ubuntu doesn't have docker
 apt-get install docker-ce docker-ce-cli 
 
 # Install unzip - Katacoda docker image doesn't have unzip
 apt-get install unzip
-cd ..
 curl -O https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
 unzip terraform_0.12.20_linux_amd64.zip -d /usr/local/bin/
 
@@ -14,9 +16,6 @@ unzip sentinel_0.15.5_linux_amd64.zip -d /usr/local/bin/
 
 # Run Docker Compose up (daemon)
 docker-compose up -d
-
-# Go back to workspace directory
-cd ~/terraform-sentinel
 
 clear
 
