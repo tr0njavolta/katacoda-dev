e257d5939b1988412157fdb955c9617eb7270553
Rachel Sharp <rachel@hashicorp.com>
2020-05-21 11:56:31 -0400

    assets

--- terraform-sentinel/courseBase.sh
+++ terraform-sentinel/courseBase.sh
@@ -4,11 +4,6 @@ cd ~/terraform-sentinel
 mkdir -p policies/test/require-fizz
 mkdir -p policies/testdata
 
-mv fail.json pass.json policies/test/require-fizz
-mv mock-tfconfig.sentinel mock-tfplan-v2-pass.sentinel mock-tfplan.sentinel mock-tfstate-v2.sentinel mock-tfconfig-v2.sentinel mock-tfplan-v2-fail.sentinel mock-tfplan-v2.sentinel mock-tfrun.sentinel mock-tfstate.sentinel policies/testdata
-mv sentinel.json policies
-mv require-fizz.sentinel policies
-
 # Install unzip - Katacoda docker image doesn't have unzip
 apt-get install unzip
 curl -O https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
