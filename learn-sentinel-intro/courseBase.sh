d21108d1e19b65117621ca50d578684f174deec6
Rachel Sharp <rachel@hashicorp.com>
2020-06-24 10:54:14 -0400

    Initial commit

---
+++ learn-sentinel-intro/courseBase.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/terraform-sentinel
+git clone https://github.com/hashicorp/hashicorp-learn-sentinel-katacoda.git ~/terraform-sentinel
+
+mv ~/restrict-s3-buckets.sentinel ~/terraform-sentinel
+
+cd ~/terraform-sentinel
+
+clear
+
+echo "Ready!"
