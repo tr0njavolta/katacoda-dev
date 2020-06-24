ab4169516db02c4f2c0c6a1743ff8507a05ced96
Rachel Sharp <rachel@hashicorp.com>
2020-06-24 12:39:39 -0400

    Update courseBase.sh

--- learn-sentinel-intro/courseBase.sh
+++ learn-sentinel-intro/courseBase.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/terraform-sentinel
 git clone https://github.com/hashicorp/hashicorp-learn-sentinel-katacoda.git ~/terraform-sentinel
-
-mv ~/restrict-s3-buckets.sentinel ~/terraform-sentinel
-
 cd ~/terraform-sentinel
+rm restrict-s3-buckets.sentinel
+
+mv ~/restrict-s3-buckets.sentinel .
 
 clear
 
