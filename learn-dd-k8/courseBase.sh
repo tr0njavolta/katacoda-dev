5e9eb444f020d46df770e06224698a26ad512e53
Rachel Sharp <rachel@hashicorp.com>
2020-10-21 17:21:48 -0400

    Update courseBase.sh

--- learn-dd-k8/courseBase.sh
+++ learn-dd-k8/courseBase.sh
@@ -2,7 +2,7 @@
 apt-get install --quiet --yes "unzip"
 
 # fetch Terraform archive
-curl --remote-name "https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip"
+wget -q https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
 
 # unzip Terraform archive and make it accessible in PATH
 unzip terraform_0.12.9_linux_amd64.zip -d "/usr/local/bin/"
