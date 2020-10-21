b5fcab12fc263c8994bc6472e171e831dcee6f97
Rachel Sharp <rachel@hashicorp.com>
2020-10-21 19:17:15 -0400

    update min

--- learn-dd-k8/courseBase.sh
+++ learn-dd-k8/courseBase.sh
@@ -2,13 +2,13 @@
 apt-get install --quiet --yes "unzip"
 
 # fetch Terraform archive
-wget -q https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
+wget -q https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.29_linux_amd64.zip
 
 # unzip Terraform archive and make it accessible in PATH
-unzip terraform_0.12.9_linux_amd64.zip -d "/usr/local/bin/"
+unzip terraform_0.12.29_linux_amd64.zip -d "/usr/local/bin/"
 
 # clean up
-rm --recursive  --force terraform_0.12.9_linux_amd64.zip
+rm --recursive  --force terraform_0.12.29_linux_amd64.zip
 
 # helm init
 helm init
