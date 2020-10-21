30cd9842652a4177d2617fdefca6d82fa59781d9
Rachel Sharp <rachel@hashicorp.com>
2020-10-21 17:11:53 -0400

    Update courseBase.sh

--- learn-dd-k8/courseBase.sh
+++ learn-dd-k8/courseBase.sh
@@ -13,7 +13,6 @@ rm --recursive  --force terraform_0.12.9_linux_amd64.zip
 # helm init
 helm init
 
-
 # add `datadog` Helm Charts (this provides `datadog/datadog:2.4.5`)
 helm repo add "datadog" "https://helm.datadoghq.com/"
 
@@ -21,8 +20,8 @@ helm repo add "datadog" "https://helm.datadoghq.com/"
 helm repo update
 
 # create user workspace
-mkdir -p workspace
+mkdir -p ~/workspace
 
-cd workspace && touch kubernetes.tf helm_datadog.tf datadog_metrics.tf datadog_synthetics.tf datadog_dashboard.tf
+cd ~/workspace && touch kubernetes.tf helm_datadog.tf datadog_metrics.tf datadog_synthetics.tf datadog_dashboard.tf
 
 echo "Ready!"
