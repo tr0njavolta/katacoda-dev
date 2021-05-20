247b4b4ce4603c96a2c9c223b80f7a473e5ac5ee
Rachel Sharp <rachel@hashicorp.com>
2021-05-20 11:32:49 -0400

    Update courseBase.sh

--- terraform-sentinel/courseBase.sh
+++ terraform-sentinel/courseBase.sh
@@ -1,3 +1,11 @@
+wget https://releases.hashicorp.com/sentinel/0.18.2/sentinel_0.18.2_linux_amd64.zip
+
+unzip sentinel_0.18.2_linux_amd64.zip
+
+rm /usr/local/bin/sentinel
+
+mv sentinel /user/local/bin
+
 mkdir -p ~/terraform-sentinel
 git clone https://github.com/tr0njavolta/2021-test-sentinel.git ~/terraform-sentinel
 
